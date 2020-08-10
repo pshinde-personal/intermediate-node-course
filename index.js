db0806901699446139fde379a27cba6031880464
Pratik Shinde <pratikshinde1497@gmail.com>
2020-08-10 18:24:07 +0530

    fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Bring in our dependencies
 const express = require('express');
 const routes = require('./app/http');
-
+const cors = require('cors');
 var app = express();
 
+app.use(cors())
 //  Connect all our routes to our application
 app.use('/', routes);
 
